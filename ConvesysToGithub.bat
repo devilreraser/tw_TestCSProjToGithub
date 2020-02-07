4a8713af3477b9e2a9a1cfb7fd748a306c5bc166
dimitar.lilov <dimitar.lilov@convesys.com>
2020-02-08 00:09:19 +0200

    n

--- ConvesysToGithub.bat
+++ ConvesysToGithub.bat
@@ -3,13 +3,19 @@ echo Process Submodules:
 cd 02_Libraries\TestCSSubmoduleToGithub
 git.exe fetch --all -v --progress
 git.exe pull --progress -v --no-rebase "origin"
-git.exe merge --allow-unrelated-histories --strategy=recursive --strategy-option=theirs remotes/convesys/master
+git.exe merge --no-ff --no-commit --allow-unrelated-histories remotes/convesys/master < ConvesysToGithub.in
+git reset HEAD .gitmodules
+git checkout -- .gitmodules
+git commit -m "merged Convesys"
 git.exe push --progress "origin" master:master
 cd ..\..
 echo Process Project:
 git.exe fetch --all -v --progress
 git.exe pull --progress -v --no-rebase "origin"
-git.exe merge --allow-unrelated-histories --strategy=recursive --strategy-option=theirs remotes/convesys/master
+git.exe merge --no-ff --no-commit --allow-unrelated-histories remotes/convesys/master < ConvesysToGithub.in
+git reset HEAD .gitmodules
+git checkout -- .gitmodules
+git commit -m "merged Convesys"
 git.exe push --progress "origin" master:master
 echo Completed!
 echo Press Enter to Exit
