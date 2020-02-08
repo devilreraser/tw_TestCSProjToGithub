cdbb7a8ba61c757c6fc9d767a38f02e6f758e9f8
dimitar.lilov <dimitar.lilov@convesys.com>
2020-02-08 19:36:30 +0200

    Empty Message

--- ConvesysToGithub.bat
+++ ConvesysToGithub.bat
@@ -3,7 +3,7 @@ echo Process Submodules:
 cd 02_Libraries\TestCSSubmoduleToGithub
 git.exe fetch --all -v --progress
 git.exe pull --progress -v --no-rebase "origin"
-git.exe merge --no-ff --allow-unrelated-histories remotes/convesys/master < ConvesysToGithub.in
+git.exe merge --no-ff --allow-unrelated-histories remotes/convesys/master < ..\..\ConvesysToGithub.in
 rem git.exe merge --no-ff --no-commit --allow-unrelated-histories remotes/convesys/master < ConvesysToGithub.in
 rem git reset HEAD gitmodules
 rem git checkout -- gitmodules
@@ -14,8 +14,8 @@ echo Process Project:
 git.exe fetch --all -v --progress
 git.exe pull --progress -v --no-rebase "origin"
 git.exe merge --no-ff --no-commit --allow-unrelated-histories remotes/convesys/master < ConvesysToGithub.in
-git reset HEAD gitmodules
-git checkout -- gitmodules
+git reset HEAD .gitmodules
+git checkout -- .gitmodules
 git commit -m "merged Convesys"
 git.exe push --progress "origin" master:master
 echo Completed!
